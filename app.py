60a9855d5ee9aaae630a8a1ef197919f9a5a7b5a
yuankraj <yuankraj@gmail.com>
2026-03-11 23:46:09 +0530

    deploy flask app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -11,5 +12,6 @@ def hello_world():
 def health():
     return 'Server is up and running'
 
+if __name__ == "__main__":
 
-app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
