cfe156d2d2a07c15956e557cd4c8eb8dec5d0789
yuankraj <yuankraj@gmail.com>
2026-03-12 01:55:05 +0530

    deploy flask app

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-EXPOSE 5000
+EXPOSE 80
 
-CMD ["python", "app.py"]
+CMD ["gunicorn","--bind", "0.0.0.0:80", "app:app"]
--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
@@ -11,7 +10,3 @@ def hello_world():
 @app.route('/health')
 def health():
     return 'Server is up and running'
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
